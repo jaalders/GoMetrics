a8a65d976a751f32dbdd8433312c72b1d60f4edc
Ahmed Barakat <ahmedbarakat@Ahmeds-MacBook-Pro.local>
2021-02-02 22:57:33 -0400

    removed navigation logic from the login service

--- src/app/login/services/login.service.ts
+++ src/app/login/services/login.service.ts
@@ -1,37 +1,36 @@
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators'
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Injectable()
 export class LoginService {
-    constructor(
-        private http: HttpClient,
-        private router: Router
-    ) { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-    validateLogin(email: string, password: string) {
-        const headers = new HttpHeaders({'Content-type': 'application/json'});
+  validateLogin(email: string, password: string): Observable<Object> {
+    const headers = new HttpHeaders({ 'Content-type': 'application/json' });
 
-        const reqObject = {
-          email: email,
-          password: password
-        };
-    
-        this.http.post('http://localhost:3000/login', reqObject, {
-            headers: headers
-          }).subscribe(
-            (response: any) => {
-              localStorage.setItem('token', response.refreshToken);
-            },
-            () => {
-              // errors should be logged through a library/framework (Ex: sentry)
-              // In addition there exception/error should handled and/or bubbled vs silent-fail
-              //console.log(error);
-            },
-            () => {
-              // This is now part of the login service.
-              this.router.navigate(["home"]);
-            }
-          )
-    }
+    const reqObject = {
+      email: email,
+      password: password
+    };
+
+    const result = this.http.post('http://localhost:3000/login', reqObject, {
+      headers: headers
+    })
+
+    // this is an observable the can be subscribed to multiple times
+    const sharedResult = result.pipe(share())
+
+    sharedResult.subscribe((response: any) => {
+      localStorage.setItem('token', response.refreshToken);
+    }, (error) => {
+      console.log(`an error has occured while calling the login endpoint, error: ${error}`)
+    })
+
+    return sharedResult
+  }
 }
